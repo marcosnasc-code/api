3c0a061077cfbc13d2a98956907360f8eb62297d
marcosnasc-code <marcos_nacimento0321@gmail.com>
2026-05-05 20:52:15 -0300

    criando as entidades

---
+++ src/main/java/com/eventos/api/domain/coupon/Coupon.java
@@ -0,0 +1,35 @@
+package com.eventos.api.domain.coupon;
+
+
+import com.eventos.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupons")
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne //tipo de relação entre as tabelas/classes Event e Coupon
+    @JoinColumn (name = "event_id") //nome da coluna que vai ser criada na tabela de coupons para referenciar a tabela de eventos
+    private Event event;
+
+
+}
